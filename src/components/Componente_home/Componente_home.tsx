fa309a07c3b41c0e77f2e4a810dd7e19bf82f908
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-07-26 10:45:54 -0300

    @GWarriorF Criação de componentes, prática de pops/hooks, criação de flexbox e grid com Tailwind CSS

---
+++ src/components/Componente_home/Componente_home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Componente_home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=913&q=80" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
