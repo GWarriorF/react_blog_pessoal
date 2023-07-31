72a5eee67ac580db304a6fca1fd7e92e89f5db90
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-07-31 12:15:23 -0300

    @GWarriorF integraçãpo com back-end, criação do CRUD de Temas/Postagens e aplicação de alertas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,14 +7,14 @@ function Footer() {
 
   return (
     <>
-        <div className="flex justify-center bg-[#ede373] text-white">
+        <div className="flex justify-center bg-[#fe79b9] text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold text-black'>Blog pessoal Generation | Copyright: </p>
-            <p className='text-lg text-black'>Acesse nossas redes sociais</p>
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+            <p className='text-lg text-white'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' color='black' />
-              <InstagramLogo size={48} weight='bold' color='black' />
-              <FacebookLogo size={48} weight='bold' color='black' />
+              <LinkedinLogo size={48} weight='bold' color='white' />
+              <InstagramLogo size={48} weight='bold' color='white' />
+              <FacebookLogo size={48} weight='bold' color='white' />
             </div>
           </div>
         </div>
